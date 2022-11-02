29b8fddb390be742cfa44699a308322993e9eac1
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-11-02 19:11:28 +0100

    created movie cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel | Movie</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-    <h1>Hello World!</h1>
+    <div id="app">
+        <div class="container">
+            @foreach ($movies as $movie)
+                <div class="movie-card">
+                    <p><strong>Titolo:</strong> {{ $movie->title }}</p>
+                    <p><strong>Titolo Originale:</strong> {{ $movie->original_title }}</p>
+                    <p><strong>Nazionalita:</strong> {{ $movie->nationality }}</p>
+                    <p><strong>Data di uscita:</strong> {{ $movie->date }}</p>
+                    <p><strong>Voto:</strong> {{ $movie->vote }}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </body>
 
 </html>
